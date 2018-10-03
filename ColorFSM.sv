c3d036eeef80a6617d0212a49c35557797e1fe0a
Ellioth <elliot22rx7@gmail.com>
2018-10-03 07:55:42 -0600

    FSM color

--- ColorFSM.sv
+++ ColorFSM.sv
@@ -15,24 +15,58 @@ module ColorFSM(input logic go, clk, reset, output logic WE1,WE2,WE3,WE4);
 	
 	always_comb
 		case (current)
-			//que voy a hacer con el estado a?
+			
+			//Cuando el go indica que se va al estado a,
+			//se puede acceder por medio del default
 			a:
 				//para la entrada go verdadera
-				if (go) begin 
+				if (go)
 					next = b;
-				end
 				// para go falsa... 
 				else next = a;
 			b:
 				next = c;
+			
+			//Cuando el go indica que se va al estado c	
+			c:
+				//para la entrada go verdadera
+				if (go)
+					next = d;
+				// para go falsa... 
+				else next = c;
+			d:
+				next = e;
+			
+			//Cuando el go indica que se va al estado e
+			e:
+				//para la entrada go verdadera
+				if (go) begin 
+					next = f;
+				end
+				// para go falsa... 
+				else next = e;
+			f:
+				next = g;
+				
+			//Cuando el go indica que se va al estado e
+			g:
+				//para la entrada go verdadera
+				if (go) begin 
+					next = h;
+				end
+				// para go falsa... 
+				else next = g;
+			h:
+				next = a;
+				
 			default: 
 				next = a;
 		endcase
 	//asignacion de la salida
 	assign WE1 = current == b;
 	assign WE2 = current == d;
-	//...
-	
+	assign WE3 = current == f;
+	assign WE4 = current == h;
 	
 		
 endmodule
